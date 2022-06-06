f92e52558f7d3556816576cebc8621455acea99c
tanishab <tanishabraganza@gmail.com>
2022-06-06 10:16:17 +0100

    added for loop

--- abcd.java
+++ abcd.java
@@ -1,6 +1,8 @@
 class abcd{
     public static void main(String args[]){
 
-        System.out.println("Hi everyone");
+        for(int i=1; i<=5; i++){
+            System.out.println("Hello");
+        }
     }
 }
